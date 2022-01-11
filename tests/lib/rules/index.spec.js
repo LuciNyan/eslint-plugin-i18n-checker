519f57b20419e70fe956de8bb1f94f240685e6a6
luci <453440680@qq.com>
2022-01-11 16:32:16 +0800

    feat: multi-json-file

--- tests/lib/rules/index.spec.js
+++ tests/lib/rules/index.spec.js
@@ -7,22 +7,22 @@ const tests = {
 
   valid: [
     { code: "t('name');" },
-    { code: "I18n.t('a.b.c');" },
-    { code: "I18n.t('d.e.f');" },
-    { code: "I18n.t(str);" },
+    { code: "i18n.t('a.b.c');" },
+    { code: "i18n.t('d.e.f');" },
+    { code: "i18n.t(str);" },
   ],
 
   invalid: [
     {
-      code: "I18n.t('just_in_en');",
+      code: "i18n.t('just_in_en');",
       errors: [
-        {message: 'Missing key: just_in_en in JSON: zh.json'},
+        {message: 'Missing key: just_in_en in JSON: /tests/locales/zh.json'},
       ],
     },
     {
-      code: "I18n.t('just_in_zh');",
+      code: "i18n.t('just_in_zh');",
       errors: [
-        {message: 'Missing key: just_in_zh in JSON: en.json'},
+        {message: 'Missing key: just_in_zh in JSON: /tests/locales/en.json'},
       ],
     },
   ],
@@ -33,10 +33,10 @@ const config = {
   options: [
     {
       functionNames: [
-        'I18n.t',
+        'i18n.t',
         't'
       ],
-      localesPath: '/tests/locales/'
+      localesPath: '/tests/locales',
     }
   ],
 };
@@ -45,3 +45,73 @@ tests.valid.forEach(t => Object.assign(t, config));
 tests.invalid.forEach(t => Object.assign(t, config));
 
 ruleTester.run("json-key-exists", rule, tests);
+
+const multiJsonConfig = {
+  options: [
+    {
+      functionNames: [
+        'i18n.t',
+        't'
+      ],
+      localesPath: '/tests/multi-json-locales',
+      specifics: [
+        {
+          matcher: /^global:(.*)$/,
+          to: 'global.json'
+        },
+        {
+          matcher: /^(.*)$/,
+          to: 'app.json'
+        }
+      ]
+    }
+  ],
+};
+
+const multiJsonTest = {
+
+  valid: [
+    { code: "t('name');" },
+    { code: "i18n.t('a.b.c');" },
+    { code: "i18n.t('d.e.f');" },
+    { code: "i18n.t(str);" },
+    { code: "t('global:name');" },
+    { code: "i18n.t('global:a.b.c');" },
+    { code: "i18n.t('global:d.e.f');" },
+    { code: "i18n.t(str);" },
+  ],
+
+  invalid: [
+    {
+      code: "t('age');",
+      errors: [
+        {message: 'Missing key: age in JSON: /tests/multi-json-locales/en/app.json'},
+        {message: 'Missing key: age in JSON: /tests/multi-json-locales/zh/app.json'},
+      ]
+    },
+    {
+      code: "t('global:age');",
+      errors: [
+        {message: 'Missing key: global:age in JSON: /tests/multi-json-locales/en/global.json'},
+        {message: 'Missing key: global:age in JSON: /tests/multi-json-locales/zh/global.json'},
+      ]
+    },
+    {
+      code: "i18n.t('global:just_in_en');",
+      errors: [
+        {message: 'Missing key: global:just_in_en in JSON: /tests/multi-json-locales/zh/global.json'},
+      ],
+    },
+    {
+      code: "i18n.t('global:just_in_zh');",
+      errors: [
+        {message: 'Missing key: global:just_in_zh in JSON: /tests/multi-json-locales/en/global.json'},
+      ],
+    },
+  ],
+};
+
+multiJsonTest.valid.forEach(t => Object.assign(t, multiJsonConfig));
+multiJsonTest.invalid.forEach(t => Object.assign(t, multiJsonConfig));
+
+ruleTester.run("json-key-exists", rule, multiJsonTest);
